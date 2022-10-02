1a6847b318e56b4d061a5081a75086a177faa719
Julia BATORO <jbatoro@e1r3p19.clusters.42paris.fr>
2022-10-02 19:39:48 +0200

    ok

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -11,34 +11,28 @@ cout is pronounced "see-out".
 -> Used for output, and uses the insertion operator (<<)
 cin is pronounced "see-in". 
 ->Used for input, and uses the extraction operator (>>)
-
 --------------------------------------------------------*/
 
 int main(int argc, char** argv)
 {
     char letter;
-    size_t i;
-    size_t j = 0;
-    string silence = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    string silence("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
 
     if (argc == 1)
     {
-        cout << silence << "\n";
+        cout << silence << endl;
         return (0);
     }
-    i = 1;
-    while ((int)i < argc)
-    {
-        j = 0;
-        while (j < strlen(argv[i]))
+    for(int i(1); i < argc ; i++)
+    {   
+        if (i < argc)
+            cout << " ";
+        for (size_t j(0); j < strlen(argv[i]); j++)
         {
             letter = toupper(argv[i][j]);
             cout << letter;
-            j++;
         }
-        i++;
-        if ((int)i < argc)
-            cout << " ";
     }
     return (0);
   }
+
