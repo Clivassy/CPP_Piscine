8292895da5990152c88743da5b71990ebf9ebd2e
Julia BATORO <jbatoro@e1r4p23.clusters.42paris.fr>
2022-10-24 16:27:09 +0200

    finished

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -10,13 +10,14 @@ int main(int argc, char** argv)
         for(int i(1); i < argc ; i++)
         {
             if (i < argc)
-                std::cout << " ";
+               std::cout << " ";
             for (size_t j(0); j < strlen(argv[i]); j++)
             {
                 letter = toupper(argv[i][j]);
                 std::cout << letter;
             }
         }  
+        std::cout << std::endl;
     }
     else
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl ;
--- module_00/ex01/phonebook.cpp
+++ module_00/ex01/phonebook.cpp
@@ -155,37 +155,43 @@ void   Phonebook::addContact( void )
     return;
 }
 
-int main(void){
+int main(int ac, char **av){
 
     std::string cmd("");
-
     Phonebook newRepertoire;
-    std::cout << YELLOW "|-------------------------------|" << std::endl;
-    std::cout << "|     WELCOME ON PHONEBOOK      |" << std::endl;
-    std::cout << "|-------------------------------|" << std::endl;
-    std::cout << "|ADD    ➜       Adding Contacts |" << std::endl;
-    std::cout << "|SEARCH ➜       Search Contact  |" << std::endl;
-    std::cout << "|EXIT   ➜       Quit program    |" << std::endl;
-    std::cout << "|-------------------------------|" CLEAR << std::endl;
-    while (!newRepertoire.exit)
+
+    (void)av;
+    if (ac == 1)
     {
-        getline(std::cin, cmd);
-        if (std::cin.eof())
+        std::cout << YELLOW "|-------------------------------|" << std::endl;
+        std::cout << "|     WELCOME ON PHONEBOOK      |" << std::endl;
+        std::cout << "|-------------------------------|" << std::endl;
+        std::cout << "|ADD    ➜       Adding Contacts |" << std::endl;
+        std::cout << "|SEARCH ➜       Search Contact  |" << std::endl;
+        std::cout << "|EXIT   ➜       Quit program    |" << std::endl;
+        std::cout << "|-------------------------------|" CLEAR << std::endl;
+        while (!newRepertoire.exit)
         {
-            exit(EXIT_FAILURE);
-        }
-        else if (!cmd.compare("ADD"))
-        {
-            newRepertoire.addContact();
-        }
-        else if (!cmd.compare("SEARCH"))
-        {
-            newRepertoire.searchContact();
-        }
-        else if (!cmd.compare("EXIT"))
-        {
-            newRepertoire.exitPhonebook(1);
+            getline(std::cin, cmd);
+            if (std::cin.eof())
+            {
+                exit(EXIT_FAILURE);
+            }
+            else if (!cmd.compare("ADD"))
+            {
+                newRepertoire.addContact();
+            }
+            else if (!cmd.compare("SEARCH"))
+            {
+                newRepertoire.searchContact();
+            }
+            else if (!cmd.compare("EXIT"))
+            {
+                newRepertoire.exitPhonebook(1);
+            }
         }
     }
+    else
+        std::cout << RED "Usage: ./phonebook" CLEAR << std::endl; 
     return (0);
 }
