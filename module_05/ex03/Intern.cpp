451e8abb71d515312960253b1677de4ed6a55b0c
Julia Batoro <juliabatoro@MacBook-Pro-de-Julia.local>
2022-11-19 09:13:20 +0100

    Ex03 in progress

--- module_05/ex03/Intern.cpp
+++ module_05/ex03/Intern.cpp
@@ -10,19 +10,58 @@ Intern::~Intern( void )
     std::cout << "Intern desstructor called" << std::endl;
 }
 
-Intern::Intern( const Intern &copy )
+Intern::Intern( const Intern& copy )
 {
-    *this = copy;
+    void(*copy);
     std::cout << "Intern constructor by copy called" << std::endl;
 }
 
 Intern &Intern::operator=( Intern const &copy )
 {
-    //
+    (void)copy;
+    return (*this);
 }
 
 Form*   Intern::makeForm(std::string FormName, std::string FormTarget)
 {
     
     std::cout << "Intern creates" << FormName << std::endl;
+}
+
+Form*	Intern::shrubbery(std::string target)
+{
+	return new ShrubberyCreationForm(target);
+}
+
+Form*	Intern::presidential(std::string target)
+{
+	return new PresidentialPardonForm(target);
+}
+
+Form*	Intern::robotomy(std::string target)
+{
+	return new RobotomyRequestForm(target);
+}
+
+Form*	Intern::makeForm(std::string form, std::string target)
+{
+	typedef Form* (Intern::*func)(std::string target);
+
+	func array[3] = {	&Intern::shrubbery,
+						&Intern::presidential,
+						&Intern::robotomy};
+
+	std::string	type[3] = { "shrubbery creation",
+							"presidential pardon",
+							"robotomy request"	};
+	for (int i = 0; i < 3; i++)
+	{
+		if (type[i] == form)
+		{
+			std::cout << "Intern create " << type[i] << " form" << std::endl;
+			return (this->*array[i])(target);
+		}
+	}
+	throw InexistingFormException();
+	return 0;
 }
--- module_05/ex03/Intern.hpp
+++ module_05/ex03/Intern.hpp
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <string>
 #include "Form.hpp"
+#include "ShrubberyCreationForm.hpp"
+#include "PresidentialPardonForm.hpp"
+#include "RobotomyRequestForm.hpp"
 
 //-------- Colors ------------
 #define CLEAR "\e[0m"
@@ -23,9 +26,12 @@ class Intern{
         Intern( const Intern &copy );
         Intern &operator=( Intern const &copy );
 
+        Form*	Intern::shrubbery(std::string target);
+        Form*	Intern::presidential(std::string target);
+        Form*	Intern::robotomy(std::string target);
         Form*   makeForm(std::string FormName, std::string FormTarget);
 
-        class InexistingFomrException : public std::exception
+        class InexistingFormException : public std::exception
         {
             public:
             virtual const char* what() const throw()
