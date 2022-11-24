f4f527cca1469796dc30148dc5a544ff2ea0c91b
Julia BATORO <jbatoro@e3r3p11.clusters.42paris.fr>
2022-11-24 18:48:23 +0100

    avancé cpp07

---
+++ module_07/ex02/Array.hpp
@@ -0,0 +1,26 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+//------ librairies------------
+#include <iostream>
+
+//-------- Colors ------------
+#define CLEAR "\e[0m"
+#define YELLOW "\e[33m"
+//----------------------------
+
+template< typename T> 
+T const & max(T const &  a, T const & b) {
+
+}
+
+template< typename T> 
+class Array {
+    public:
+
+    private:
+    unsigned int size;
+    T *Array;
+
+}
+#endif
